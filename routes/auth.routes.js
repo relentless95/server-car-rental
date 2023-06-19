9c249954f99f6965bcb8b227498072f8bf9da735
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-06-19 22:07:32 +0200

    updated login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -81,14 +81,44 @@ router.post("login", (req, res, next) => {
 
   // check if email or password are provided as empty string
   if (email === "" || password === "") {
-    res.status(400).json({ message: "Provide username and password." });
+    res.status(400).json({ message: "Provide email and password." });
     return;
   }
-});
 
-// Check the users collection if a user with the same email exists
-user.findOne({email})
-.then((foundUser))
+  // Check the users collection if a user with the same email exists
+  User.findOne({ email })
+    .then((foundUser) => {
+      if (!foundUser) {
+        // if the user is not found, send an error
+        res.status(401).json({ message: "User not found." });
+        return;
+      }
+
+      // compare the provided password with the one saved in the database
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+      if (passwordCorrect) {
+        //Deconstruct the user object to omit the password
+        const { _id, email, name } = foundUser;
+
+        //Create an object that will be set as the token payload
+        const payload = { _id, email, name };
+
+        // Create and sign the token
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiredIn: "6h",
+        });
+        console.log(authToken);
+
+        //send the token as the response
+        res.status(200).json({ authToken: authToken });
+      } else {
+        res.status(401).json({ message: "Unable to authenticate the User" });
+      }
+    })
+    .catch((err) => res.status(500).json({ message: "Internal Server Error" }));
+});
 
 // Get /api/users - get request to test the route
 router.get("/users", (req, res, next) => {
